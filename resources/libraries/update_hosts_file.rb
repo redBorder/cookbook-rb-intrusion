2b469f855e3d34ec1f488465e8edec2a1a8e8f1f
ljblancoredborder <ljblanco@redborder.com>
2025-05-14 14:10:20 +0100

    add node names, but check that is being duplicated because of the read

--- resources/libraries/update_hosts_file.rb
+++ resources/libraries/update_hosts_file.rb
@@ -4,6 +4,7 @@ module RbIps
       Chef::DataBag.load('rBglobal').keys.grep(/^ipvirtual-external-/).map { |bag| bag.sub('ipvirtual-external-', '') }
     end
 
+    # Is weird to read the file to update it, just because we want not to remove unmanaged system configuration
     def read_hosts_file
       hosts_hash = Hash.new { |hash, key| hash[key] = [] }
       File.readlines('/etc/hosts').each do |line|
@@ -13,17 +14,24 @@ module RbIps
         services = values
         hosts_hash[ip].concat(services).uniq!
       end
-      hosts_hash
+      hosts_hash # IP => [services and nodes]
+    end
+
+    def manager_node_names
+      query = Chef::Search::Query.new
+      nodes = []
+      query.search(:node, 'is_manager:true') do |node|
+        nodes << node.name + '.node'
+      end
+      nodes
     end
 
     def update_hosts_file
       manager_registration_ip = node['redborder']['manager_registration_ip'] if node['redborder'] && node['redborder']['manager_registration_ip']
-
-      return unless manager_registration_ip
+      return unless manager_registration_ip # Can be also virtual ip
 
       running_services = node['redborder']['systemdservices'].values.flatten if node['redborder']['systemdservices']
       databags = get_external_databag_services
-      hosts_hash = read_hosts_file
 
       # Hash where services (from databag) are grouped by ip
       grouped_virtual_ips = Hash.new { |hash, key| hash[key] = [] }
@@ -39,10 +47,12 @@ module RbIps
       end
 
       # Add running services to localhost
+      # Why only localhost is being grouped?
       grouped_virtual_ips['127.0.0.1'] ||= []
       running_services.each { |serv| grouped_virtual_ips['127.0.0.1'] << serv }
 
       # Group services
+      hosts_hash = read_hosts_file
       grouped_virtual_ips.each do |new_ip, new_services|
         new_services.each do |new_service|
           # Avoids having duplicate services in the list
@@ -54,10 +64,10 @@ module RbIps
           # Add running services to localhost
           if new_ip == '127.0.0.1' && running_services.include?(new_service)
             hosts_hash['127.0.0.1'] << "#{new_service}.service"
-            next
+            next #service
           end
 
-          # If there is a virtual ip and ips is manager mode
+          # If there is a virtual ip and IPS is manager mode
           if new_ip && !node['redborder']['cloud']
             hosts_hash[new_ip] << "#{new_service}.service"
             hosts_hash[new_ip] << "#{new_service}.#{node['redborder']['cdomain']}"
@@ -68,7 +78,10 @@ module RbIps
         end
       end
 
+      hosts_hash[manager_registration_ip] = (hosts_hash[manager_registration_ip] || []) + manager_node_names
+
       # Prepare the lines for the hosts file
+      # TODO: include in host entries manager-.node when cluster is implemented
       hosts_entries = []
       hosts_hash.each do |ip, services|
         format_entry = format('%-18s%s', ip, services.join(' '))
