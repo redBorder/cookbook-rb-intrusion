5b5d2330080947ffb169760190c1010b94d3f4d3
ljblancoredborder <ljblanco@redborder.com>
2025-07-04 11:26:01 +0100

    sfacctd service is not needed to connect with ips. Excluded

--- resources/libraries/update_hosts_file.rb
+++ resources/libraries/update_hosts_file.rb
@@ -3,7 +3,10 @@
 module RbIps
   module Helpers
     def external_databag_services
-      Chef::DataBag.load('rBglobal').keys.grep(/^ipvirtual-external-/).map { |bag| bag.sub('ipvirtual-external-', '') }
+      external_databag = Chef::DataBag.load('rBglobal').keys.grep(/^ipvirtual-external-/)
+      services = external_databag.map { |bag| bag.sub('ipvirtual-external-', '') }
+      services -= %w[sfacctd] # not visible for ips
+      services
     end
 
     # Is weird to read the file to update it, just because we want not to remove unmanaged system configuration
@@ -29,76 +32,6 @@ module RbIps
         nodes << "#{node.name}.node"
       end
       nodes
-    end
-
-    # This function can be deprecated since we started to control /etc/hosts with gather_hosts_info()
-    def update_hosts_file
-      unless node.dig('redborder', 'resolve_host')
-        domain_name = node.dig('redborder', 'manager_registration_ip')
-        return if domain_name.nil?
-        resolved_ip = manager_to_ip(domain_name)
-        return if resolved_ip.nil?
-        node.normal['redborder']['resolve_host'] = resolved_ip
-      end
-      manager_registration_ip = node.dig('redborder', 'resolve_host')
-
-      running_services = node['redborder']['systemdservices'].values.flatten if node['redborder']['systemdservices']
-      databags = external_databag_services
-
-      # Hash where services (from databag) are grouped by ip
-      grouped_virtual_ips = Hash.new { |hash, key| hash[key] = [] }
-      databags.each do |bag|
-        virtual_dg = data_bag_item('rBglobal', "ipvirtual-external-#{bag}")
-        ip = virtual_dg['ip']
-
-        if ip && !ip.empty?
-          grouped_virtual_ips[ip] << bag.gsub('ipvirtual-external-', '')
-        else
-          grouped_virtual_ips[manager_registration_ip] << bag.gsub('ipvirtual-external-', '')
-        end
-      end
-
-      # Add running services to localhost
-      # Why only localhost is being grouped?
-      grouped_virtual_ips['127.0.0.1'] ||= []
-      running_services.each { |serv| grouped_virtual_ips['127.0.0.1'] << serv }
-
-      # Group services
-      hosts_hash = read_hosts_file
-      grouped_virtual_ips.each do |new_ip, new_services|
-        new_services.each do |new_service|
-          # Avoids having duplicate services in the list
-          service_key = new_service.split('.').first
-          hosts_hash.each_value do |services|
-            services.delete_if { |service| service.split('.').first == service_key }
-          end
-
-          # Add running services to localhost
-          if new_ip == '127.0.0.1' && running_services.include?(new_service)
-            hosts_hash['127.0.0.1'] << "#{new_service}.service"
-            next
-          end
-
-          # If there is a virtual ip and IPS is manager mode
-          if new_ip && !node['redborder']['cloud']
-            hosts_hash[new_ip] << "#{new_service}.service"
-            hosts_hash[new_ip] << "#{new_service}.#{node['redborder']['cdomain']}"
-          else # Add services with manager_registration_ip
-            hosts_hash[manager_registration_ip] << "#{new_service}.service"
-            hosts_hash[manager_registration_ip] << "#{new_service}.#{node['redborder']['cdomain']}"
-          end
-        end
-      end
-
-      hosts_hash[manager_registration_ip] = (hosts_hash[manager_registration_ip] || []) + manager_node_names
-
-      # Prepare the lines for the hosts file
-      hosts_entries = []
-      hosts_hash.each do |ip, services|
-        format_entry = format('%-18s%s', ip, services.join(' '))
-        hosts_entries << format_entry unless services.empty?
-      end
-      hosts_entries
     end
 
     def add_localhost_info(hosts_info)
