1e6e96727858e4d9f6f68c4904def9cf0bb8dae2
ljblancoredborder <ljblanco@redborder.com>
2025-10-15 15:35:02 +0100

    migarte from ips: rework the methods and add nginx services (webui)

--- resources/libraries/update_hosts_file.rb
+++ resources/libraries/update_hosts_file.rb
@@ -2,26 +2,38 @@
 
 module RbIps
   module Helpers
-    def get_external_databag_services
-      # This function turned too complex ...
-      # external_databag = Chef::DataBag.load('rBglobal').keys.grep(/^ipvirtual-external-/)
-      # services = external_databag.map { |bag| bag.sub('ipvirtual-external-', '') }
-      # services -= %w[sfacctd nginx] # not visible for ips
-      # services += ['webui'] # nginx is not visible for ips, while webui is redirected to nginx on the manager
-      # services
+    require 'resolv'
 
-      # ... so we hardcode the list of services for now
-      %w(f2k kafka webui)
+    def external_databag_services
+      # This function turned too complex ...
+      external_databag = Chef::DataBag.load('rBglobal').keys.grep(/^ipvirtual-external-/)
+      services = external_databag.map { |bag| bag.sub('ipvirtual-external-', '') }
+      services -= %w(sfacctd) # Excluded for IPS
+      services
     end
 
-    # Is weird to read the file to update it, just because we want not to remove unmanaged system configuration
-    # or data written from rb_register_url.sh or system_health_check.rb
+    def grouped_virtual_ips(manager_registration_ip)
+      # Hash where services (from databag) are grouped by ip
+      grouped_virtual_ips = Hash.new { |ip, services| ip[services] = [] }
+      # Only those visible for IPS. Manager will recognize these services as nginx
+      # But erchef and s3 are necessary to access VIPs, so moved to implicit
+      nginx_services = %w(http2k webui)
+
+      external_databag_services.each do |bag|
+        data = data_bag_item('rBglobal', "ipvirtual-external-#{bag}")
+        ip = data['ip'] || manager_registration_ip
+        service_name = bag.delete_prefix('ipvirtual-external-')
+
+        services = service_name == 'nginx' ? nginx_services : [service_name]
+        grouped_virtual_ips[ip] |= services
+      end
+      grouped_virtual_ips
+    end
+
     def read_hosts_file
-      # This function is deprecated
       hosts_hash = Hash.new { |hash, key| hash[key] = [] }
       File.readlines('/etc/hosts').each do |line|
         next if line.strip.empty? || line.start_with?('#')
-
         values = line.split(/\s+/)
         ip = values.shift
         services = values
@@ -60,16 +72,16 @@ module RbIps
     end
 
     def add_manager_services_info(hosts_info, manager_registration_ip, cdomain)
-      # This services are critical for the use of chef to rewrite the hosts file
-      implicit_services = ['erchef.service']
-      if cdomain
-        implicit_services << "erchef.service.#{cdomain}"
-        implicit_services << "s3.service.#{cdomain}"
-      end
-
       # Services not contained in node information
+      implicit_services = [
+        'erchef.service',
+        "erchef.service.#{cdomain}",
+        's3.service',
+        "s3.service.#{cdomain}"
+      ]
+
       other_services = if cdomain
-                         %w(data http2k).map { |s| "#{s}.#{cdomain}" }
+                         ['data', 'rbookshelf.s3'].map { |s| "#{s}.#{cdomain}" }
                        else
                          []
                        end
@@ -78,18 +90,8 @@ module RbIps
     end
 
     def add_virtual_ips_info(hosts_info, manager_registration_ip, cdomain)
-      # Hash where services (from databag) are grouped by ip
-      grouped_virtual_ips = Hash.new { |hash, key| hash[key] = [] }
-      get_external_databag_services.each do |bag|
-        virtual_dg = data_bag_item('rBglobal', "ipvirtual-external-#{bag}")
-        ip = virtual_dg['ip']
-        ip = ip && !ip.empty? ? ip : manager_registration_ip
-        grouped_virtual_ips[ip] << bag.gsub('ipvirtual-external-', '')
-      end
-
       is_mode_manager = !node['redborder']['cloud']
-      grouped_virtual_ips.each do |ip, services|
-        services.uniq! # Avoids having duplicate services in the list
+      grouped_virtual_ips(manager_registration_ip).each do |ip, services|
         services.each do |service|
           # Add running services to localhost
           next if ip == '127.0.0.1'
@@ -115,7 +117,6 @@ module RbIps
         node.normal['redborder']['resolve_host'] = resolved_ip
       end
       manager_registration_ip = node.dig('redborder', 'resolve_host')
-
       return {} unless manager_registration_ip
 
       cdomain = node.dig('redborder', 'cdomain')
@@ -124,8 +125,7 @@ module RbIps
       hosts_info = add_localhost_info(hosts_info)
       hosts_info = add_manager_names_info(hosts_info, manager_registration_ip, cdomain)
       hosts_info = add_manager_services_info(hosts_info, manager_registration_ip, cdomain)
-      # hosts_info = add_virtual_ips_info(hosts_info)
-      add_virtual_ips_info(hosts_info, manager_registration_ip, cdomain)
+      add_virtual_ips_info(hosts_info, manager_registration_ip, cdomain) # returns hosts_info
     end
   end
 end
